ca5321e23bac3ba8423224d61dddc9c7b8e12e65
BILLIE <billie@ubuntu.(none)>
2014-09-12 00:59:35 +0800

    order capability

--- front/page/order.php
+++ front/page/order.php
@@ -24,6 +24,11 @@ class Front_Page_Index extends Front_Page {
 	/* Public Methods
 	-------------------------------*/
 	public function render() {
+		if (!empty($_POST)) {
+			front()->orders()->create($_POST['lname'], $_POST['fname'], $_POST['mi'],
+			 $_POST['address'], $_POST['ig'], $_POST['fb'], $_POST['cno'], $_POST['mop'], $_POST['dop']);
+			header("Location:\\");
+		}
 		return $this->_page();
 	}
 	
